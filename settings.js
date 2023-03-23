5d4fa029fbf7920bebf11c661bf043160fd0f78c
HMUdara <109807584+HMUdara@users.noreply.github.com>
2023-03-23 16:22:04 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -14,7 +14,7 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.ANTI_BADWORD = 'true' // true of false
+global.ANTI_BADWORD = 'false' // true of false
 global.AUTO_REACT = 'true' // true of false
 global.INBOX_BLOCK_MSG ='BLOCK !!!'
 global.INBOX_BLOCK = 'off' //inbox block
